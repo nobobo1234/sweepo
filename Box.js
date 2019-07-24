c4fd36fe8264f54af0113a3e1b98cfe77c5c83ac
nobobo1234 <noahvanboven@gmail.com>
2019-07-24 14:37:44 +0200

    Adding numbers

--- Box.js
+++ Box.js
@@ -4,11 +4,35 @@ class Box {
         this.y = y;
         this.length = length;
         this.isBomb = isBomb;
-        this.isFlagged = false;
+        this.neighbors = [];
+        this.bombs = 0;
         this.isChosen = false;
     }  
 
+    addNeighbors(field) {
+        for (let k = -1; k < 2; k++) {
+            for (let m = -1; m < 2; m++) {
+                if(this.x + k > 0 && this.y + m > 0 && this.x + k < field.length && this.y + m < field[0].length && abs(k) + abs(m) > 0) {
+                    this.neighbors.push(field[this.x+k][this.y+m]);
+                }
+            }
+        }
+        this.bombs = this.neighbors.filter(box => box.isBomb).length;
+    }
+
+    reveal() {
+        this.isChosen = true;
+        if(this.bombs === 0) {
+            for(const neighbor of this.neighbors) {
+                if(!neighbor.isBomb && neighbor.bombs === 0) neighbor.reveal();
+            }
+        }
+    }   
+
     show() {
+        const x = this.x * this.length;
+        const y = this.y * this.length;
+        push();
         if(this.isChosen && this.isBomb) {
             fill(0);
         } else if(this.isChosen) {
@@ -17,6 +41,14 @@ class Box {
             fill(255);
         }
         stroke(0);
-        rect(this.x, this.y, 30, 30);
+        rect(x, y, this.length, this.length);
+        // Draw the text
+        if(this.isChosen) {
+            fill(0);
+            strokeWeight(0);
+            textAlign(CENTER, CENTER);
+            text(`${this.bombs ? this.bombs : ''}`, x + this.length / 2, y + this.length / 2);
+        }
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const fields = [];
+const field = [];
 const squareLength = 30; // Length of a square in the field in pixels
 
 
@@ -6,23 +6,28 @@ function setup() {
     createCanvas(windowWidth/2, windowHeight);
     
     for(let i = 0; i < (width-squareLength)/squareLength; i++) {
-        fields.push([]); // Add a new column to the 2d array
+        field.push([]); // Add a new column to the 2d array
         for(let j = 0; j < (height-squareLength)/squareLength; j++) {
-            const x = i*squareLength;
-            const y = j*squareLength;
+            const x = i;
+            const y = j;
             let isBomb = false;
 
             if(random() < 0.1) isBomb = true;
 
-            fields[i][j] = new Box(x, y, squareLength, isBomb); // Add a new 'Box' to the field
+            field[i][j] = new Box(x, y, squareLength, isBomb); // Add a new 'Box' to the field
         }
     }
+
+    for (const box of field.flat()) {
+        box.addNeighbors(field);
+    }
+    console.log('Done');
 }
 
 function draw() {
     background(255);
 
-    for(const box of fields.flat()) {
+    for(const box of field.flat()) {
         box.show();
     }
 }
@@ -30,10 +35,13 @@ function draw() {
 function mousePressed() {
     const x = floor(mouseX / squareLength);
     const y = floor(mouseY / squareLength);
-    fields[x][y].isChosen = true;
-    if(fields[x][y].isBomb) {
-        for(const box of fields.flat()) {
-            box.isChosen = true;
+    if(x < field.length && y < field[0].length) {
+        field[x][y].isChosen = true;
+        // If you touch a bomb, you're game over
+        if(field[x][y].isBomb) {
+            for(const box of field.flat()) {
+                box.isChosen = true;
+            }
         }
     }
 }
