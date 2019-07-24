793bec58390cd45bf332efa0b875998a13ffbd6e
nobobo1234 <noahvanboven@gmail.com>
2019-07-24 12:29:35 +0200

    Improving field, adding mousePressed

--- Box.js
+++ Box.js
@@ -1,7 +1,22 @@
 class Box {
-    constructor(isBomb) {
+    constructor(x, y, length, isBomb) {
+        this.x = x;
+        this.y = y;
+        this.length = length;
         this.isBomb = isBomb;
         this.isFlagged = false;
         this.isChosen = false;
     }  
+
+    show() {
+        if(this.isChosen && this.isBomb) {
+            fill(0);
+        } else if(this.isChosen) {
+            fill(200);
+        } else {
+            fill(255);
+        }
+        stroke(0);
+        rect(this.x, this.y, 30, 30);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -5,22 +5,35 @@ const squareLength = 30; // Length of a square in the field in pixels
 function setup() {
     createCanvas(windowWidth/2, windowHeight);
     
-    for(let i = 0; i < width-squareLength; i+=squareLength) {
-        for(let j = 0; j < height-squareLength; j+=squareLength) {
-            fields.push(new Box(false));
+    for(let i = 0; i < (width-squareLength)/squareLength; i++) {
+        fields.push([]); // Add a new column to the 2d array
+        for(let j = 0; j < (height-squareLength)/squareLength; j++) {
+            const x = i*squareLength;
+            const y = j*squareLength;
+            let isBomb = false;
+
+            if(random() < 0.1) isBomb = true;
+
+            fields[i][j] = new Box(x, y, squareLength, isBomb); // Add a new 'Box' to the field
         }
     }
 }
 
 function draw() {
-    background(51);
+    background(255);
 
-    for(let i = 0; i < width-squareLength; i+=squareLength) {
-        for(let j = 0; j < height-squareLength; j+=squareLength) {
-            stroke(255);
-            strokeWeight(1);
-            fill(51);
-            rect(i, j, squareLength, squareLength);
+    for(const box of fields.flat()) {
+        box.show();
+    }
+}
+
+function mousePressed() {
+    const x = floor(mouseX / squareLength);
+    const y = floor(mouseY / squareLength);
+    fields[x][y].isChosen = true;
+    if(fields[x][y].isBomb) {
+        for(const box of fields.flat()) {
+            box.isChosen = true;
         }
     }
 }
